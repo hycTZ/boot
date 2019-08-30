35ea685a9eec607aaadf390bc29b3d41e67dd7e3
hycTZ <377957770@qq.com>
2019-08-30 17:34:51 +0800

    TJ

---
+++ src/main/java/com/core/web/Controller.java
@@ -0,0 +1,17 @@
+package com.core.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boot")
+public class Controller {
+
+
+    @RequestMapping("/test")
+    public String test(){
+
+        return "TEST-OK";
+    }
+}
