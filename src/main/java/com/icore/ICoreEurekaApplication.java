bf030ea98a2a140916bcbdd8b20f949b923696bf
hycTZ <377957770@qq.com>
2019-08-30 16:46:21 +0800

    TJ

---
+++ src/main/java/com/icore/ICoreEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.icore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICoreEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ICoreEurekaApplication.class, args );
+    }
+}
