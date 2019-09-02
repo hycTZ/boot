eb9702ba1ee47541baaa0450db065e3087a618c7
hycTZ <377957770@qq.com>
2019-09-02 11:28:22 +0800

    TJ

--- src/main/java/com/core/web/Controller.java
+++ src/main/java/com/core/web/Controller.java
@@ -1,17 +1,18 @@
 package com.core.web;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/boot")
 public class Controller {
-
-
+    @Value("${name}")
+    String name;
     @RequestMapping("/test")
     public String test(){
 
-        return "TEST-OK";
+        return "TEST-OK:" + name;
     }
 }
